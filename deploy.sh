53aa45674117cb86c3e98e448c577671eaa73f04
ryan <1184518756@qq.com>
2020-10-27 10:28:33 +0800

    update script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git reset --hard origin/master
 yarn
 yarn run docs:build
 rm -rf /usr/share/nginx/html/file/docs
-cp ./docs/.vuepress/dist /usr/share/nginx/html/file/docs
+cp ./docs/.vuepress/dist /usr/share/nginx/html/file/docs -r
